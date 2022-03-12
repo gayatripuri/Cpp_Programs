935bedf7e30bd33d450b145c237e29e6ce322ac5
Gayatri Puri <92669468+gayatripuri@users.noreply.github.com>
2022-03-12 21:20:49 +0530

    Create ASCII value of char.cpp

---
+++ ASCII value of char.cpp
@@ -0,0 +1,18 @@
+//to find ASCII value of character
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  
+ char a;
+ cout<<"enter character:"<<endl;
+ cin>>a;
+ cout<<"ascii value of "<<a<<" is "<<int(a);
+ return 0;
+    
+    
+    
+    
+}
